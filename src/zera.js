5871fc8280dcadd1259ed74e2ebedb79304b6129
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-03-04 12:21:46 -0800

    after 2 days

--- src/zera.js
+++ src/zera.js
@@ -4,14 +4,14 @@ function Zera(){
     const myStyle = {
         color : 'green'
     }
-    const {data, ispending, error} = useFetch("http://localhost:9000/blogss") 
+    const {data, ispending, error} = useFetch("http://localhost:9000/blogs") 
      
     return(
         <div>
             {error && <div style={myStyle}>{error}</div>}
             {ispending && <div style={myStyle}>Loadding...</div>}
             <div>
-            <h1>Users List</h1>
+            <h1>Users List and Description</h1>
             <ul>
                 {data.map((a) => (
                     <div className="zera-box">
