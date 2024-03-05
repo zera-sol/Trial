6cfcd22c6fca10d80e715a8363a12983da5b8392
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-03-05 10:49:45 -0800

    1st commit

--- src/Header.js
+++ src/Header.js
@@ -7,27 +7,27 @@ import Message from "./pages/message";
 const logo = require('./compnents/logo192.png') 
       export default function Header(){
          return(
-         <div className="header">
-            <img className="logo" src={logo} alt="my_logo"/>
-            <ul>
-               <Switch>
-                  <Route path="/" exact component={Home} >
-                   <li>Home</li>
-                  </Route>
-                  <Route path="/cart" exact component={Cart} >
-                    <li>Chart</li>
-                  </Route>
-                  <Route path="/contact" exact component={Contact} >
-                    <li>Contact</li>
-                  </Route>
-                  <Route path="/messege" exact component={Message} >
-                    <li>Message</li>
-                  </Route>
-               </Switch>
+            <Router>
+               <div className="header">
+                  <img className="logo" src={logo} alt="my_logo"/>
+                  <ul>
+                     <Switch>
+                        <Route path="/" exact component={Home} >
+                        <li>Home</li>
+                        </Route>
+                        <Route path="/cart" exact component={Cart} >
+                        <li>Chart</li>
+                        </Route>
+                        <Route path="/contact" exact component={Contact} >
+                        <li>Contact</li>
+                        </Route>
+                        <Route path="/messege" exact component={Message} >
+                        <li>Message</li>
+                        </Route>
+                     </Switch>               
+                  </ul>
+               </div>
                
-               
-               
-               
-            </ul>
-         </div>)
+            </Router>
+        )
       }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import Header from "./Header";
 import Body from "./Body";
+import "./index.css"
 import Meme from "./Meme";
 import Counter from "./counter";
 import Form from "./form";
@@ -10,12 +10,6 @@ import Zera from "./zera";
 
 ReactDOM.render(
     <div>
-      <Switch>
-         <Route path="/" exact component={Form} ></Route>
-         <Route path="/header" exact component={Header} ></Route>
-         <Route path="/meme" exact component={Form} ></Route>
-         <Route path="/" exact component={Form} ></Route>
-      </Switch>
        <Header />
        <Zera />
        <Form />
