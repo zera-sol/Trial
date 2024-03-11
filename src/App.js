6d2df6527bf1d9abb9d69a89fc0d6095bf6af67b
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-03-11 04:10:57 -0700

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,55 +1,14 @@
 import React from "react";
 import "./index.css"
-import Navbar from "./compnents/Navbar";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
-import AddBlog from "./pages/blogs";
-import Home from "./pages/Home"
-import Blog from "./compnents/blog";
-import {useState }from "react"
-import useFetch from "./useFetch";
-
+import NavBar from "./compnents/Nav";
+import Hero from "./compnents/Hero";
 
 function App(){
-let data = useFetch("http://localhost:9000/Blogs")
-let [display, setDisplay] = useState(false)
-let [id, setId] = useState("")
-function findId(obj){
-    setId(obj.id)
-    setDisplay(true)
-}
-        let home = data.map((a) => (
-            <Home 
-                key={a.id}
-                title={a.title}
-                author={a.author}
-                detail={a.detail}
-                handleClick={()=>findId(a)}
-            />       
- ))
-        let pass = id;
-        let detail = data.filter((a) => a.id === pass).map((a) => (
-            
-            <Blog 
-                key={a.id}
-                title={a.title}
-                author={a.author}
-                detail={a.detail}
-                />
-        ))
-    return (
-        <Router>
-            <div>
-                <Navbar />
-                <Switch>
-                    <Route exact path="/">
-                        {display ? detail : home}
-                    </Route>
-                    <Route path="/blog">
-                        <AddBlog />
-                    </Route>
-                </Switch>
-            </div>        
-        </Router>
+    return(
+    <div>
+       <NavBar />
+       <Hero />
+    </div>
     )
 }  
 
---
+++ src/compnents/Hero.js
@@ -0,0 +1,21 @@
+export default function Hero(){
+    return(
+        <div className="hero">
+            <div className="img-section">
+                <img src="../images/IMG_20240311_135919_774.jpg" alt=""/>
+            </div>
+            <div className="hero_text" >
+                <h4>Hey</h4>
+                <h2>I am Zerihun Solomon</h2>
+                <h3>Full-Stack <span>Software Developer</span></h3>
+                <p>A highly motivated student studying Artificial Intelligence and Machine Learning. Actively seeking opportunities to apply my skills and knowledge to real-world projects and contribute to the advancement of the industry.</p>
+            </div>
+            <div>
+                <ul>
+                    <li>Learn more</li>
+                    <li><a href="/contact">Contact Me</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
