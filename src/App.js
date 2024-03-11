2c68db609fd680d7a810a6e0751e700fca2f7d13
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-03-11 06:26:42 -0700

    commit 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./index.css"
 import NavBar from "./compnents/Nav";
 import Hero from "./compnents/Hero";
+import Button from "./compnents/button";
 
 function App(){
     return(
     <div>
        <NavBar />
        <Hero />
+       <Button />
     </div>
     )
 }  
---
+++ src/compnents/button.js
@@ -0,0 +1,10 @@
+export default function Button(){
+    return(
+        <div className="buttons">
+            <ul>
+                <li><a href="/about">Learn More</a></li>
+                <li><a href="/contact">Contact Me</a></li>
+            </ul>
+        </div>
+    )
+}
