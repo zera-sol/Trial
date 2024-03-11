29a84aa2a82a6635407365619e3c670015fa1055
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-03-11 10:41:33 -0700

    commit 5

--- src/compnents/Hero.js
+++ src/compnents/Hero.js
@@ -1,4 +1,12 @@
+import React from "react"
+import { Cursor, useTypewriter } from "react-simple-typewriter"
 export default function Hero(){
+    const [text] = useTypewriter({
+        words: ['Software developer', 'Ui/Ux designer', 'Freelancer'],
+        loop:{},
+        typeSpeed:100,
+        deleteSpeed:120
+    })
     return(
         <div className="hero">
             <div>
@@ -8,9 +16,8 @@ export default function Hero(){
             </div>
             <div>
                 <div className="hero_text" >
-                    <h4>Hey</h4>
-                    <h2>I'm Zerihun Solomon</h2>
-                    <h3>Full-Stack <span>Software Developer</span></h3>
+                    <h4>Hey, this is Zerihun</h4>
+                    <h2>I'm a <span>{text} <Cursor /></span></h2>
                     <p>A highly motivated student studying Artificial Intelligence and Machine Learning. Actively seeking opportunities to apply my skills and knowledge to real-world projects and contribute to the advancement of the industry.</p>
                 </div>
             </div>         
--- src/compnents/Nav.js
+++ src/compnents/Nav.js
@@ -2,13 +2,15 @@ import React  from "react"
 export default function NavBar(){
     return(
         <div className="nav_bar">
-            <ul>
-                <li><a href='/contact'>Contact</a></li>
-                <li><a href='/resume'>Resume</a></li>
-                <li><a href='/about'>About</a></li>
-                <li><a href='/certificate'>Certificate</a></li>
-                <li><a href='/projects'>Projects</a></li>
-            </ul>
+            <div>
+                <ul>
+                    <li><a href='/contact'>Contact</a></li>
+                    <li><a href='/resume'>Resume</a></li>
+                    <li><a href='/about'>About</a></li>
+                    <li><a href='/certificate'>Certificate</a></li>
+                    <li><a href='/projects'>Projects</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
