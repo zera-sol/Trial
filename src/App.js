e2831b263720bb5ace4a17ea6e22e230e23e6a7a
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-03-24 23:44:29 -0700

    commit 1

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ import Hero from "./compnents/zera";
 import RenderServices from "./servicerender";
 
 function App(){
-    const toasStyle = {
-         height: "100vh",
-         width: "300px",
-         marginRight: "100px",
-         display: "flex",
-         justifyContent: "center",
-         alignItems: "center",
-         backgroundColor: "#2FB4CE"
-    }
     return(
     <div className="boo">
        <NavBar />
---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHPcWmKJSDh5iJgqi0vw2hIHaVOaF46Fw",
+  authDomain: "zera-sol-portfolio.firebaseapp.com",
+  projectId: "zera-sol-portfolio",
+  storageBucket: "zera-sol-portfolio.appspot.com",
+  messagingSenderId: "279423528438",
+  appId: "1:279423528438:web:21e4d2e624de8b43692bd7",
+  measurementId: "G-FY269CHMCR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
