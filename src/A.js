0ebd29923b1c0fa7a959cba283431e87ff9dd69d
Zerihun Solomon Siyum <133159202+zera-sol@users.noreply.github.com>
2024-04-05 22:50:00 +0300

    Update A.js

--- src/A.js
+++ src/A.js
@@ -11,8 +11,9 @@ function Notification({ message, onClose }) {
   );
 }
 function findLargestNumber(arr){
+  let max= 0;
       for(let i=0; i<arr.length; i++){
-        
+        if(arr[i] > max){}
       }
 }
 function MyComponent() {
