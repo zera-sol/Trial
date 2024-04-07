ce8d8a2421ecbbdfcfa102fbc5168b21480b5ac5
Zerihun Solomon Siyum <133159202+zera-sol@users.noreply.github.com>
2024-04-07 15:12:13 +0300

    Update A.js

--- src/A.js
+++ src/A.js
@@ -32,6 +32,11 @@ function fibonacci(num){
 console.log(fibonacci(5))
 function finMinimumNumber(arr){
   let minNumber = arr[0]
+  for(let i = 1; i< arr.length; i++){
+    if(arr[i] < minNumber){
+      minNumber = arr[i]
+    }
+  }
 }
 function MyComponent() {
   const [showNotification, setShowNotification] = useState(false);
