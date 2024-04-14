ddbf2d73ff02ba720d316dacecd4c76c1d49f267
Zerihun Solomon Siyum <133159202+zera-sol@users.noreply.github.com>
2024-04-14 20:29:19 +0300

    Update A.js

--- src/A.js
+++ src/A.js
@@ -17,10 +17,10 @@ function ommitRepeation(str){
   }
   for(let i = 1; i < arr.length; i++){
     if(arr[i] === arr[i - 1]){
-      arr.splice(arr[i], 1)
+      arr.splice(i, 1)
     }
   }
-  return arr;
+  return arr.join("");
 }
 function countDigits(number) {
     // Convert the number to a string
